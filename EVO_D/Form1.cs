fa48ed896c286dac0f9810f108ee59ed23b34720
Mohamed <Mohamed@EXCLUSION>
2019-03-13 15:19:58 +0200

    s

--- EVO_D/Form1.cs
+++ EVO_D/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace EVO_D
 {
-    public partial class Form1 : MetroFramework.Forms.MetroForm
+    public partial class Form1 : Form
     {
         public Form1()
         {
